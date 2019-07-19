340c2b3c8364b75807c53b4dbec146a2b586cbc5
mustafatufan <mustafa@tufan.ee>
2019-07-19 17:44:49 +0300

    Spring Boot initial

---
+++ src/main/java/ee/tufan/log/LogSignerApplication.java
@@ -0,0 +1,13 @@
+package ee.tufan.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogSignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogSignerApplication.class, args);
+	}
+
+}
